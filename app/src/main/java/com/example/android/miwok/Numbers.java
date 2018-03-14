50c78160bb1de701d48e6c24d340db0bef3de4ba
Alexei Kravchuk <alexey.k@iard.co.il>
2018-03-14 18:14:42 +0200

    I am in Lesson 2.17 before ArrayAdapter

--- app/src/main/java/com/example/android/miwok/Numbers.java
+++ app/src/main/java/com/example/android/miwok/Numbers.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Numbers extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class Numbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+//        String [] words = new String[10];
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+//        Log.v("NumbersActivity", "The word[0] is: " + words.get(0));
+//        Log.v("NumbersActivity", "The word[5] is: " + words.get(5));
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//        TextView word = new TextView(this);
+//        word.setText(words.get(0));
+//        rootView.addView(word);
+
+
+        for (int i = 0; i < words.size(); i++) {
+            TextView word = new TextView(this);
+            word.setText(words.get(i));
+            rootView.addView(word);
+        }
     }
 }
