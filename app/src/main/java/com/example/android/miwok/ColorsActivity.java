939410aa091b2fc114c8633906c58255a14167a0
lesha K <alexei.jobs@gmail.com>
2018-03-24 16:24:35 +0300

    Add dicts to all activities

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,38 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red","weṭeṭṭi"));
+        words.add(new Word("green","chokokki"));
+        words.add(new Word("brown","ṭakaakki"));
+        words.add(new Word("gray","ṭopoppi"));
+        words.add(new Word("black","kululli"));
+        words.add(new Word("white","kelelli"));
+        words.add(new Word("dusty yellow","ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+
+        // Find the root view so we can add child views to it
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,words);
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
     }
 }
