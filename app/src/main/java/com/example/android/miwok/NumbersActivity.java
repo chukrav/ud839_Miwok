4933ff44ea9f01d5cb392ca4944afd0c157c7279
lesha K <alexei.jobs@gmail.com>
2018-04-07 23:20:59 +0300

    Add release MPlayer in NumAct

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -27,6 +27,20 @@ import java.util.ArrayList;
 public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
 
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null){
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,16 +48,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "luti", R.drawable.number_one,R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two,R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three,R.raw.number_three));
-        words.add(new Word("four", "oyyisa", R.drawable.number_four,R.raw.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five,R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six,R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven,R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight,R.raw.number_eight));
-        words.add(new Word("nine", "wo'e", R.drawable.number_nine,R.raw.number_nine));
-        words.add(new Word("ten", "na'aacha", R.drawable.number_ten,R.raw.number_ten));
+        words.add(new Word("one", "luti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
 
         // Find the root view so we can add child views to it
 //        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
@@ -57,9 +71,11 @@ public class NumbersActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
                 Word word = words.get(i);
+                releaseMediaPlayer();
                 //mMediaPlayer = MediaPlayer.create(NumbersActivity.this,R.raw.number_one);
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this,word.getAudioResourceId());
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
                 mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
 //                Toast.makeText(NumbersActivity.this,"List item clicked!",Toast.LENGTH_SHORT).show();
             }
         });
